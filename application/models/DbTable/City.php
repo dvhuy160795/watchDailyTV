50a9d89677c0fa411a9e7fdd5fb476b844045ecf
Dao Van Huy <huydv@vnext.com.vn>
2018-01-25 16:45:23 +0700

    create model city,district

---
+++ application/models/DbTable/City.php
@@ -0,0 +1,16 @@
+<?php
+
+class Application_Model_DbTable_City extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'wtv_city';
+
+    public function getCityByConditionAnd (&$aryUser = null , $condition = null) {
+    	
+    }
+
+    public function getCityByConditionOR (&$aryUser = null , $condition = null) {
+    	
+    }
+
+}
---
+++ application/models/DbTable/District.php
@@ -0,0 +1,16 @@
+<?php
+
+class Application_Model_DbTable_District extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'wtv_district';
+
+    public function getDistrictByConditionAnd (&$aryUser = null , $condition = null) {
+    	
+    }
+
+    public function getDistrictByConditionOR (&$aryUser = null , $condition = null) {
+    	
+    }
+
+}
